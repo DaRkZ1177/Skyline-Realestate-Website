1cb7eae3601935f977db1094dd606e0c5a3c7bad
DaRkZ1177 <mandyalpaarth55@gmail.com>
2022-12-04 14:57:12 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,8 +55,8 @@ function scrollActive(){
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
-        sectionTop = current.offsetTop - 58,
-        sectionId = current.getAttribute('id')
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
